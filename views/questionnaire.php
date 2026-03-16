dc167da73de7ada666eec046315b575ce75957b6
alimkaf <alimkaf4@gmail.com>
2026-03-16 11:52:01 +0100

    questionnaire

--- views/questionnaire.php
+++ views/questionnaire.php
@@ -1,5 +1,9 @@
 <?php 
 session_start(); 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 //Supprimer les indicateurs en session dès l’arrivée sur la page
 unset(
     $_SESSION['QR'],
